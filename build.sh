a5f92bd10b5a3a1a02c9c7ad75278af63b2a44a8
Berto Yáñez <bertez@gmail.com>
2014-03-11 13:37:14 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-zip -r ../dev_gen.zip *
-cd ..
+if [ -d "build" ]; then
+    rm -rfv build
+fi
+
+mkdir build
+zip -r build/dev_gen.zip * --exclude "*build*" --exclude "*.pyc" --exclude "README.md"
+cd build
 echo '#!/usr/bin/env python' | cat - dev_gen.zip > dev_gen
 rm dev_gen.zip
 chmod +x dev_gen
